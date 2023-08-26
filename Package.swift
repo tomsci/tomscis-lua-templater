b0c15edde1b0bd0f1c6ef6857f7fcc4b3aea5d08
Tom Sutcliffe <tomsci@me.com>
2023-08-26 12:47:29 +0100

    Exclude templater_tests.lua from compiled-in modules

--- Package.swift
+++ Package.swift
@@ -33,6 +33,9 @@ let package = Package(
             dependencies: [
                 .product(name: "Lua", package: "LuaSwift")
             ],
+            exclude: [
+                "src/templater_tests.lua",
+            ],
             plugins: [
                 .plugin(name: "EmbedLuaPlugin", package: "LuaSwift")
             ]
